6050ba622c4d1332fe70b19bb2923e245eb054cd
Emmanuel Correa <ebcorreac@gmail.com>
2023-03-21 18:12:55 -0300

    agregando elementos de forma automatica

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const containerPad = document.getElementsByClassName('container');
-const rowPad = document.getElementsByClassName('row');
-const btnPads = document.querySelectorAll('container-pads');
-const data = ['A','B','C','D','E','F', 'G','H'];
+const rowPad = document.getElementById('rowPad');
+const botones = {
+    boton1: 'A',
+    boton2: 'B',
+    boton3: 'C',
+    boton4: 'D',
+    boton5: 'E',
+    boton6: 'F',
+    boton7: 'G',
+    boton8: 'B'
+  };
 
-
-const selectNotePad = (note) => {
+function selectNotePad (note) {
     // Como puede hacer qeu por medio de esta funcion suene un audio
+    console.log(note);
+    
+}
+createBtnPad(botones);
+
+function createBtnPad(btnPad){
+    for (const key in btnPad) {
+        const buttonPads  = document.createElement('button');
+        buttonPads.className = 'col-3 container-notes';
+        buttonPads.type = 'button'
+        buttonPads.id = key;
+        // buttonPads.onclick = selectNotePad(btnPad[key]);
+        buttonPads.onclick = function() {
+            console.log(`Haz hecho clic en el botón ${btnPad[key]}`);
+        };
+        buttonPads.textContent = btnPad[key];
+        rowPad.appendChild(buttonPads);
+    }
 }
 
 const selectModePad = (mode) => {
 
 }
-
