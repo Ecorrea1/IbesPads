091119dccac4b24a5f7af344ba99e13b51a9bb7c
Emmanuel Correa <ebcorreac@gmail.com>
2023-03-21 17:30:35 -0300

    agregando cositas

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const containerPad = document.getElementsByClassName('container');
 const rowPad = document.getElementsByClassName('row');
+const btnPads = document.querySelectorAll('container-pads');
+const data = ['A','B','C','D','E','F', 'G','H'];
+
+
+const selectNotePad = (note) => {
+    // Como puede hacer qeu por medio de esta funcion suene un audio
+}
+
+const selectModePad = (mode) => {
+
+}
+
